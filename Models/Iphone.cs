c921ed801fe22dba8513a630b0fd941f0af191e2
Leonardo buta <teste@teste.com>
2023-12-28 23:57:14 -0300

    Adicionar arquivos de projeto.

---
+++ Models/Iphone.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesafioPOO.Models
+{
+    // TODO: Herdar da classe "Smartphone"
+    public class Iphone { }
+    
+        namespace DesafioPOO.Models
+    {
+        // TODO: Herdar da classe "Smartphone"
+        public class Iphone : Smartphone
+        {
+            // TODO: Sobrescrever o método "InstalarAplicativo"
+            public Iphone(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
+            {
+
+            }
+
+            public override void InstalarAplicativo(string nomeApp)
+            {
+                Console.WriteLine($"Instalando o aplicativo \"{nomeApp}\" no Iphone");
+            }
+        }
+    }
+}
---
+++ Models/Nokia.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesafioPOO.Models
+{
+    // TODO: Herdar da classe "Smartphone"
+    public class Nokia { }
+    
+       namespace DesafioPOO.Models
+    {
+        // TODO: Herdar da classe "Smartphone"
+        public class Nokia : Smartphone
+        {
+            // TODO: Sobrescrever o método "InstalarAplicativo"
+            public Nokia(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
+            {
+
+            }
+
+            public override void InstalarAplicativo(string nomeApp)
+            {
+                Console.WriteLine($"Instalando o aplicativo \"{nomeApp}\" no Nokia");
+            }
+        }
+    }
+}
+
